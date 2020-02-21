a5dc3b8e5eb90a0ae51c4c25822a4de0ca5aa953
Fredrik Vaagen <fredva@stud.ntnu.no>
2020-02-21 06:27:05 +0100

    added documentation

--- source/orders.h
+++ source/orders.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @brief Organizes and handles the orders, and current and next destination.
+ */
+
 #include <stdbool.h>
 
 int current_floor;
@@ -8,8 +13,23 @@ bool order_up[HARDWARE_NUMBER_OF_FLOORS]; //kunne vært størrelse floors-1 men 
 bool order_down[HARDWARE_NUMBER_OF_FLOORS];
 //void set_current_mission();
 
+
+
+/**
+ * @brief Checks if there are any new orders and adds them to @c order_inside , @c order_up and @c order_down . Turns on the respective button lights as well.
+ * 
+ */
 void set_orders();
 
+/**
+ * @brief Clears order from floor @p floor of type @p order_type. Turns off respective button lights as well.
+ * 
+ * @param floor floor
+ * @param order_type HARDWARE_ORDER_UP or HARDWARE_ORDER_DOWN or HARDWARE_ORDER_INSIDE
+ */
 void clear_order(int floor, HardwareOrder order_type);
 
+/**
+ * @brief Clears all orders and turns off all button lights.
+ */
 void clear_all_orders();
